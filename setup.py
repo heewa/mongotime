b7e7aee80d6992b0f5be0db2eb9fbad66dcb090b
Heewa Barfchin <heewa.b@gmail.com>
2018-02-05 15:25:17 -0500

    Update setup.py for pypi publishing

--- setup.py
+++ setup.py
@@ -8,12 +8,26 @@ here = path.abspath(path.dirname(__file__))
 # https://packaging.python.org/guides/single-sourcing-package-version/
 exec(open(path.join(here, 'mongotime', 'version.py')).read())
 
+LONG_DESC = '''
+Mongotime is a sampling-based performance analysis tool for MongoDB that aims
+to give you a deep view into how Mongo is spending its time by showing %
+utilization of various activity types, and allowing you to add custom grouping
+and filtering.
+
+This approach is particularly useful if your DB is strained by a high volume of
+fast queries, rather than a few slow ones. Besides optimizing slow queries, you
+might want to understand and change general access patterns. For example, maybe
+your DB is spending way too much time looking up a user's last login time, even
+if each lookup is very fast because of indexing.
+'''
+
 setup(
     name='mongotime',
     version=__version__,  # noqa
     description=(
         'Sampling Profiler for Mongo DB - see what the DB spends time on'),
     url='http://github.com/heewa/mongotime',
+    download_url='https://github.com/heewa/mongotime/archive/v0.1.0.tar.gz',
     author='Heewa Barfchin',
     author_email='heewa.b@gmail.com',
     license='MIT',
@@ -49,4 +63,4 @@ setup(
         'pytest',
         'tox',
     ],
-    long_description='TODO')
+    long_description=LONG_DESC)
